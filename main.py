0e408f5c23eebc27efeb1fcf81617e8cba4e7d2c
vantab1ack9days <shaldeevv@gmail.com>
2025-10-24 17:39:14 +0700

    MASLOVA

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("ДИМА ТЫ НЕ ПРАВ")
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+  if request.method == 'POST':
+    username = request.form['username']
+    email = request.form['email']
+
+    # Обработка данных формы (например, сохранение в базе данных)
+    print(f"Имя пользователя: {username}, Email: {email}")
+
+    return "Данные успешно отправлены!"
+  else:
+    return render_template('index.html')
+
+if __name__ == '__main__':
+  app.run(debug=True)
