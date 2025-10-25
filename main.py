88c47818b5cceab2da8eb90731c030bdd1fe28f1
vantab1ack9days <shaldeevv@gmail.com>
2025-10-25 13:45:35 +0700

    тыцн дыцн

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
-# main.py
-
-from flask import Flask, request
+from flask import Flask, render_template, request, url_for
+import random
 
 app = Flask(__name__)
 
-@app.route("/calc")
-def calculator():
-    a = int(request.args.get('a'))
-    b = int(request.args.get('b'))
-    op = request.args.get('op')
-    if op=='add':
-        return f'Result is {a+b}'
-    elif op=='diff':
-        return f'Result is {a-b}'
-    if op=='mult':
-        return f'Result is {a*b}'
-    if op=='sub':
-        return f'Result is {a/b}'
-    
+@app.route("/", methods=["GET", "POST"])
+def main():
+    if request.method == 'POST':
+        input_num = request.form['phone_num']
+        return number(input_num)
+    else:
+        nums = []
+        for _ in range(1000):
+            tmp = '89'
+            for __ in range(9):
+                symb = str(random.randint(1, 9))
+                tmp += symb
+            nums.append(tmp)
+        return render_template('template.html', nums = nums)
 
-
+@app.route("/?number=<num>")
+def number(num):
+    return render_template('number.html', num=num)
 
 if __name__ == "__main__":
     app.run(debug=True)
-    # app.run(host='10.42.0.1', port=5000)
